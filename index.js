07a9ef9a3ce6e9ed81fc691fd75b8eabdabdfdfb
abhay282104 <abhisavaliya2004gmail.com>
2025-07-15 11:02:47 +0530

    js basic pro

---
+++ index.js
@@ -0,0 +1,32 @@
+// 1. Check Even or Odd
+
+// const num = 10;
+
+// if (num % 2 == 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+
+// // 2. Find the Maximum of Two Numbers
+
+// const num1 = 10;
+// const num2 = 20;
+
+// if (num1 > num2) {
+//   console.log(num1);
+// } else {
+//   console.log(num2);
+// }
+
+// 3. Check Leap Year
+//  let year=2022
+//     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) 
+//     {
+//         console.log("leap yr");
+//     } else {
+//         console.log("not leap yr");
+//     }
+
+// 4. Sum of Natural Numbers
+
